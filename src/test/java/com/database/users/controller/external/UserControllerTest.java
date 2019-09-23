a48fe004068d99a482333db9cd3a918145b4c76d
Maciej Gumieniak <maciej.gumieniak@comarch.com>
2019-09-23 15:50:06 +0200

    Add ErrorAdvice

---
+++ src/test/java/com/database/users/controller/external/UserControllerTest.java
@@ -0,0 +1,29 @@
+package com.database.users.controller.external;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void getUserById() {
+
+    }
+
+    @Test
+    void getUsers() {
+    }
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void patchUser() {
+    }
+}
