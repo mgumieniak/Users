83feccd6e5763c7d5242eeef155e8218cc451dc7
Maciej Gumieniak <maciejgumieniak007@gmail.com>
2019-09-23 21:00:15 +0200

    Add: TemplateMethod Save

--- src/main/java/com/database/users/error/UserErrorAdvice.java
+++ src/main/java/com/database/users/error/UserErrorAdvice.java
@@ -5,23 +5,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class UserErrorAdvice {
 
-    @ExceptionHandler({RuntimeException.class})
-    public ResponseEntity<String>handleRunTimeException(RuntimeException ex){
-        return error(HttpStatus.INTERNAL_SERVER_ERROR, ex);
-    }
+//    @ExceptionHandler({RuntimeException.class})
+//    public ResponseEntity<String>handleRunTimeException(RuntimeException ex){
+//        return error(HttpStatus.INTERNAL_SERVER_ERROR, ex);
+//    }
 
     @ExceptionHandler({UserNotFoundException.class})
-    public ResponseEntity<String>handleUserNotFoundException(UserNotFoundException ex){
-        return error(HttpStatus.NOT_FOUND, ex);
+    public ResponseEntity<Error>handleUserNotFoundException(UserNotFoundException ex){
+        return error(HttpStatus.NOT_FOUND, ex, "UserNotFoundException.class");
     }
 
-    private ResponseEntity<String> error(HttpStatus status, Exception e) {
+    private ResponseEntity<Error> error(HttpStatus status, Exception e, String exceptionName) {
         log.error("Exception : ", e);
-        return ResponseEntity.status(status).body(e.getMessage());
+        Error error = Error.createError(status.value(),
+                e.getMessage(), exceptionName);
+        return ResponseEntity.status(status).body(error);
     }
 }
--- src/main/java/com/database/users/error/UserNotFoundException.java
+++ src/main/java/com/database/users/error/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.database.users.error;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(String message) {
