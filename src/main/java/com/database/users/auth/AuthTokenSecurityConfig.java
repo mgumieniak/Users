30173b0d0f64381e984eae18e2fbd8f569579619
Maciej Gumieniak <maciej.gumieniak@comarch.com>
2019-09-24 16:05:19 +0200

    Init commit

---
+++ src/main/java/com/database/users/auth/AuthTokenSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.database.users.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class AuthTokenSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private String authHeaderName ="AUTH_API_KEY";
+
+    private String authHeaderValue = "123";
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        PreAuthTokenHeaderFilter filter = new PreAuthTokenHeaderFilter(authHeaderName);
+
+        filter.setAuthenticationManager(authentication -> {
+            String principal = (String) authentication.getPrincipal();
+
+            if (!authHeaderValue.equals(principal)) {
+                throw new BadCredentialsException("The API key was not found "
+                        + "or not the expected value.");
+            }
+            authentication.setAuthenticated(true);
+            return authentication;
+        });
+
+        httpSecurity.
+                antMatcher("/api/**")
+                .csrf()
+                .disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(filter)
+                .addFilterBefore(new ExceptionTranslationFilter(
+                                new Http403ForbiddenEntryPoint()),
+                        filter.getClass()
+                )
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+
+}
---
+++ src/main/java/com/database/users/auth/SpringSecurityInitializer.java
@@ -0,0 +1,8 @@
+package com.database.users.auth;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SpringSecurityInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+    //no code needed
+}
